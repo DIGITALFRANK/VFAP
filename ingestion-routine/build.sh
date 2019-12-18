3cd2819ba79d3bcc0dfd05aeed6bc340046f78d3
Bhanu Reddy <Bhanu_Reddy@vfc.com>
2019-12-18 15:04:16 -0500

    all changes in dev

--- ingestion-routine/build.sh
+++ ingestion-routine/build.sh
@@ -5,4 +5,4 @@ mv $CODEBUILD_SRC_DIR/utils/* $CODEBUILD_SRC_DIR/ingestion-routine/src/
 ls $CODEBUILD_SRC_DIR/ingestion-routine/src/
 find $CODEBUILD_SRC_DIR/ -iname template.json > $CODEBUILD_SRC_DIR/param-resolver/src/templates.txt
 python3 $CODEBUILD_SRC_DIR/param-resolver/src/param-resolver.py
-zip -r9 $CODEBUILD_SRC_DIR/ingestion-routine/src/ingestion-routine.zip $CODEBUILD_SRC_DIR/param-resolver/src/* 
+zip -r9 $CODEBUILD_SRC_DIR/ingestion-routine/src/ingestion-routine.zip $CODEBUILD_SRC_DIR/ingestion-routine/src/* 
