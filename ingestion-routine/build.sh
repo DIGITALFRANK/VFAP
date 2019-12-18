2f56fae97fe7a6508274e4fef0419044ab25740c
Bhanu Reddy <Bhanu_Reddy@vfc.com>
2019-12-18 15:41:12 -0500

    troubleshooting

--- ingestion-routine/build.sh
+++ ingestion-routine/build.sh
@@ -1,9 +1,11 @@
 # Build commands for ingestion routine
 
-pip install -r requirements.txt --target $CODEBUILD_SRC_DIR/ingestion-routine/src/
+# pip install -r requirements.txt --target $CODEBUILD_SRC_DIR/ingestion-routine/src/
 mv $CODEBUILD_SRC_DIR/utils/* $CODEBUILD_SRC_DIR/ingestion-routine/src/
 find $CODEBUILD_SRC_DIR/ -iname template.json > $CODEBUILD_SRC_DIR/param-resolver/src/templates.txt
 cd $CODEBUILD_SRC_DIR/param-resolver/src/
+cat $CODEBUILD_SRC_DIR/param-resolver/src/templates.txt
+ls
 python3 param-resolver.py
-zip -r9 $CODEBUILD_SRC_DIR/ingestion-routine/src/ingestion-routine.zip $CODEBUILD_SRC_DIR/ingestion-routine/src/*
-aws s3 sync $CODEBUILD_SRC_DIR/ s3://vf-artifacts-bucket/vfap/
+# zip -r9 $CODEBUILD_SRC_DIR/ingestion-routine/src/ingestion-routine.zip $CODEBUILD_SRC_DIR/ingestion-routine/src/*
+# aws s3 sync $CODEBUILD_SRC_DIR/ s3://vf-artifacts-bucket/vfap/
