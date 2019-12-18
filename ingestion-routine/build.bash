ab59a2d259ec7c09150fa0575c98a1167bffa41b
Bhanu Reddy <Bhanu_Reddy@vfc.com>
2019-12-18 14:31:21 -0500

    updated buildspec.yml

---
+++ ingestion-routine/build.bash
@@ -0,0 +1,9 @@
+# Build commands for ingestion routine
+
+mv $CODEBUILD_SRC_DIR/utils/* $CODEBUILD_SRC_DIR/ingestion-routine/src/
+ls $CODEBUILD_SRC_DIR/ingestion-routine/src/
+sam build -t $CODEBUILD_SRC_DIR/ingestion-routine/templates/template.yml
+sam package --template-file $CODEBUILD_SRC_DIR/ingestion-routine/templates/template.yml --s3-bucket vf-artifacts-bucket --s3-prefix vfap/artifacts/ --output-template-file $CODEBUILD_SRC_DIR/ingestion-routine/templates/template-packaged.json --use-json
+find $CODEBUILD_SRC_DIR/ -iname template-packaged.json > $CODEBUILD_SRC_DIR/param-resolver/src/templates.txt
+ls $CODEBUILD_SRC_DIR/ingestion-routine/templates/
+python3 $CODEBUILD_SRC_DIR/param-resolver/src/param-resolver.py
