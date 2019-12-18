9c283b0065798b0a37f653eacf43204a16142905
Bhanu Reddy <Bhanu_Reddy@vfc.com>
2019-12-18 15:48:47 -0500

    troubleshooting

--- ingestion-routine/build.sh
+++ ingestion-routine/build.sh
@@ -4,8 +4,8 @@
 mv $CODEBUILD_SRC_DIR/utils/* $CODEBUILD_SRC_DIR/ingestion-routine/src/
 find $CODEBUILD_SRC_DIR/ -iname template.json > $CODEBUILD_SRC_DIR/param-resolver/src/templates.txt
 cd $CODEBUILD_SRC_DIR/param-resolver/src/
-cat $CODEBUILD_SRC_DIR/param-resolver/src/templates.txt
 python3 param-resolver.py
+cp $CODEBUILD_SRC_DIR/ingestion-routine/templates/template-resolved.json $CODEBUILD_SRC_DIR/
 ls -R $CODEBUILD_SRC_DIR/ingestion-routine/
 # zip -r9 $CODEBUILD_SRC_DIR/ingestion-routine/src/ingestion-routine.zip $CODEBUILD_SRC_DIR/ingestion-routine/src/*
 # aws s3 sync $CODEBUILD_SRC_DIR/ s3://vf-artifacts-bucket/vfap/
