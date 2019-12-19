522b4bf4c36f332ca2bc65734e19880d4c95f135
Bhanu Reddy <Bhanu_Reddy@vfc.com>
2019-12-19 10:16:08 -0500

    adding main.json

--- ingestion-routine/build.sh
+++ ingestion-routine/build.sh
@@ -5,6 +5,6 @@ mv $CODEBUILD_SRC_DIR/utils/* $CODEBUILD_SRC_DIR/ingestion-routine/src/
 find $CODEBUILD_SRC_DIR/ -iname template.json > $CODEBUILD_SRC_DIR/param-resolver/src/templates.txt
 cd $CODEBUILD_SRC_DIR/param-resolver/src/
 python3 param-resolver.py
-cp $CODEBUILD_SRC_DIR/ingestion-routine/templates/template-resolved.json $CODEBUILD_SRC_DIR/
 zip -r9 $CODEBUILD_SRC_DIR/ingestion-routine/src/ingestion-routine.zip $CODEBUILD_SRC_DIR/ingestion-routine/src/*
 aws s3 cp $CODEBUILD_SRC_DIR/ingestion-routine/src/ingestion-routine.zip s3://vf-artifacts-bucket/vfap/ingestion-routine/src/
+aws s3 sync $CODEBUILD_SRC_DIR/ingestion-routine/templates/*  s3://vf-artifacts-bucket/vfap/ingestion-routine/templates/
