34e45e8d3ee4b3da7ccb5c74de472dcad9f97744
Bhanu Reddy <Bhanu_Reddy@vfc.com>
2020-02-27 17:44:52 -0500

    testing the semantic versioning 3

--- sem.py
+++ sem.py
@@ -1,8 +1,12 @@
 import semantic_version
+import boto3
 
 # M - Major
 # m - Minor
 # p - patch
+sm_client = boto3.client('secretsmanager')
+response = client.get_secret_value(SecretId='versioning')
+print(response['SecretString'])
 
 version = semantic_version.Version('0.1.1')
 increment = "M"
