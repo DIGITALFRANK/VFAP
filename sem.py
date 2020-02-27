fd9e799dfb7092ae8cd6be4f04a0cd70a7d4714c
Bhanu Reddy <Bhanu_Reddy@vfc.com>
2020-02-27 18:17:14 -0500

    testing the semantic versioning 6

--- sem.py
+++ sem.py
@@ -5,7 +5,16 @@ import json
 # M - Major
 # m - Minor
 # p - patch
+
 sm_client = boto3.client('secretsmanager')
+
+def update_version(new_version):
+	secretString="{\"version\": \"" + new_version + "\"}"
+	response = client.update_secret(
+		SecretId='versioning',
+		SecretString=secretString
+	)
+
 response = sm_client.get_secret_value(SecretId='versioning')
 i  = json.loads(response['SecretString'])
 version = i['version']
@@ -15,10 +24,13 @@ version = semantic_version.Version(version)
 increment = "M"
 if increment == "M":
 	new_version = version.next_major()
+	update_version(new_version)
 	print(str(new_version))
 elif increment == "m":
 	new_version = version.next_minor()
+	update_version(new_version)
 	print(str(new_version))
 elif increment == "p":
 	new_version = version.next_patch()
+	update_version(new_version)
 	print(str(new_version))
