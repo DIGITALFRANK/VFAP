aa2f5af1dcb9f577bc7ae88fc8ed4036551f7338
Bhanu Reddy <Bhanu_Reddy@vfc.com>
2020-02-28 12:50:58 -0500

    commited new config files env specific

--- config-files/build.sh
+++ config-files/build.sh
@@ -5,4 +5,11 @@ set -ue
 echo "Syncing the artifacts"
 aws s3 sync $CODEBUILD_SRC_DIR/config-files/ s3://vf-artifacts-bucket/vfap/config-files/
 
+### temporary solution till we figure out the solution
+### Copying the envrionment specific config_store.ini file
+
+#aws s3 cp $CODEBUILD_SRC_DIR/config-files/dev/config_store.ini s3://vf-artifacts-bucket/vfap/config-files/dev/config_store.ini
+aws s3 cp $CODEBUILD_SRC_DIR/config-files/qa/config_store.ini s3://vf-artifacts-bucket/vfap/config-files/qa/config_store.ini
+# aws s3 cp $CODEBUILD_SRC_DIR/config-files/prod/config_store.ini s3://vf-artifacts-bucket/vfap/config-files/prod/config_store.ini
+
 echo "Build.sh completed"
