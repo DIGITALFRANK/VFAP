134728588185fe52c0a025a73727eb55061ad84c
Bhanu Reddy <Bhanu_Reddy@vfc.com>
2020-02-28 23:24:47 -0500

    final fix 7

--- versioning/semantic-version-v2.py
+++ versioning/semantic-version-v2.py
@@ -20,16 +20,3 @@ update_type = str(sys.argv[2])
 
 print(current_version)
 print(update_type)
-
-version = semantic_version.Version(current_version)
-
-if update_type == "M":
-	new_version = version.next_major()
-elif update_type == "m":
-	new_version = version.next_minor()
-elif update_type == "p":
-	new_version = version.next_patch()
-else:
-	print("[ERROR]: Vaild update type is M - major, m - minor and p - patch")
-
-print(str(new_version))
